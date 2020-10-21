34c2d6b20163e0693e1a7f8e5b0118ec9bb14174
vincent-ngo31 <vincent.ngo31@berkeley.edu>
2020-10-21 01:43:06 -0700

    'about' update

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
               src="images/profilepic.jpg"
               alt="Vincent Ngo Profile Pic"
             />
-            <div className="seals">
+            {/* <div className="seals">
               <img
                 className="berkeley-seal"
                 src="images/berk.png"
@@ -22,29 +22,29 @@ class About extends Component {
                 src="images/gtech.png"
                 alt="georgia-tech-seal"
               />
-            </div>
+            </div> */}
           </div>
           <div className="nine columns main-col">
             <h2>About Me</h2>
             <p>
-              Hi! I'm Vincent, a San Diego native, a spicy food obsessive, and
-              an avid curator of sad music playlists.
+              Hi! I'm Vincent, a San Diego native, a recent UC Berkeley graduate
+              with a double major in Data Science and Cognitive Science, an
+              incoming Masters student at Georgia Tech, and a software engineer
+              at Roblox.
             </p>
-            <p>
+            {/* <p>
               I graduated from UC Berkeley with a double major in Data Science
               and Cognitive Science in May 2020, and I was recently admitted to
               Georgia Tech's Master of Computer Science program for Spring 2021.
-            </p>
+            </p> */}
             <p>
-              I'm hoping to apply my skills and knowledge to a Software
-              Engineering role in the near future. I'm constantly looking for
-              ways to improve myself, and I'm not afraid to get my hands dirty
-              to prove it. I perform best when I'm surrounded with ambitious,
-              like-minded people who are just as passionate about their work as
-              I am. When I'm not working on my next project, I'm probably
-              enjoying an oolong milk tea or discovering underrated artists on
-              Spotify. Please feel free to check out some of my past projects
-              below.
+              I'm constantly looking for ways to improve myself, and I'm not
+              afraid to get my hands dirty to prove it. I perform best when I'm
+              surrounded with ambitious, like-minded people who are just as
+              passionate about their work as I am. When I'm not working on my
+              next project, I'm probably enjoying an oolong milk tea or
+              discovering underrated artists on Spotify. Please feel free to
+              check out some of my past projects below.
             </p>
           </div>
         </div>
